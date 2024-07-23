1d0a56d86483e323cb0735fc69080d834dfac3c9
jsalicetti <jessicasalicetti@yahoo.com>
2024-07-23 12:25:33 -0400

    completed

---
+++ PA303.10.1/src/FireMonster.java
@@ -0,0 +1,10 @@
+public class FireMonster  extends  Monster{
+    public FireMonster(String name) {
+        super(name);
+    }
+
+    @Override
+    String attack(){
+        return "Attack with fire!";
+    }
+}
---
+++ PA303.10.1/src/Monster.java
@@ -0,0 +1,9 @@
+public class Monster {
+    String name;
+    Monster(String name){
+        this.name = name;
+    }
+    String attack(){
+        return "!^_&^$@+%$* I don't know how to attack!";
+    }
+}
